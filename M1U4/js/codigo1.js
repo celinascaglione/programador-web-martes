8350583f886163d8b174ef0467a8f5877bb5cba5
Celina Scaglione <celinascaglione@hotmail.com>
2023-05-01 02:35:42 -0300

    Tarea,ejercicio1

---
+++ M1U4/js/codigo1.js
@@ -0,0 +1,16 @@
+const distancia= prompt ('Ingrese a qué distancia se encuentra:', '');
+if(distancia>100000){
+    alert('Te recomiendo ir en avión')
+}else
+if(distancia>30000){
+    alert('Te recomiendo ir en auto')
+}else
+    if(distancia>10000){
+       alert('Te recomiendo ir en colectivo') 
+    }else
+        if(distancia>1000){
+            alert('Te recomiendo ir en bicicleta')
+        }else{
+            alert('Te recomiendo ir a pie')
+        }
+    
