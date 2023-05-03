58544bd5376bc49a6be02917250ecf7de1c6d959
Celina Scaglione <celinascaglione@hotmail.com>
2023-05-03 14:46:54 -0300

    tarea 1 y 2

---
+++ M1U4/codigo2.js
@@ -0,0 +1,22 @@
+
+
+
+//const vector = [5, 8, 3, 11, 9, 7, 6];
+var num = [5, 8, 3, 11, 9, 7, 6]
+console.log(num.length);
+console.log(num);
+
+let mayor = 0;
+
+for (let i=0; i<num.length; i++){
+if(num[i] > mayor) {
+    mayor = num[i]
+}
+}
+    console.log('El mayor es el:' + mayor);
+    alert('El número mayor es: ' + mayor);
+
+
+
+
+
